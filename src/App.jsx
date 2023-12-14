a0584cf802c0e671de9393f61a5e22fcc53e6b52
AkshayB99 <akshaybharamshetti99@gmail.com>
2023-12-14 10:35:46 +0530

    Some changes in alignment

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,27 @@ import Home from "./components/Home";
 import Elements from "./components/Elements";
 
 function App() {
+  // This code will attempt to prevent the user from navigating back
+  history.pushState(null, null, location.href);
+  window.onpopstate = function () {
+    history.go(1);
+  };
+
   const [showHeaders, setShowHeaders] = useState(false);
   console.log(showHeaders);
   return (
     <>
       <div className="main-container">
-      <div className={showHeaders ? "left-container600" : "left-container"}>
-          <Headers setShowHeaders={setShowHeaders}  />
+        <div className={showHeaders ? "left-container600" : "left-container"}>
+          <Headers setShowHeaders={setShowHeaders} />
         </div>
         <div className={showHeaders ? "right-container600" : "right-container"}>
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/data" element={<Elements setShowHeaders={setShowHeaders} />} /> 
+            <Route
+              path="/data"
+              element={<Elements setShowHeaders={setShowHeaders} />}
+            />
           </Routes>
         </div>
       </div>
